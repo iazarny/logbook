870227df805100a62ce6fd1de8b91e36e4e8c6bd
Igor Azarny <iazarny@yahoo.com>
2020-04-20 15:55:35 +0300

    Mail integration

--- src/main/java/com/az/lb/MailConfiguration.java
+++ src/main/java/com/az/lb/MailConfiguration.java
@@ -14,19 +14,8 @@ import java.util.Properties;
 @Configuration
 public class MailConfiguration {
 
-    @Value("${lb.mail.sender.host}")
-    private String lbMailSenderHost;
 
-    @Value("${lb.mail.sender.port}")
-    private int lbMailSenderPort;
-
-    @Value("${lb.mail.sender.username}")
-    private String lbMailSenderUsername;
-
-    @Value("${lb.mail.sender.password}")
-    private String lbMailSenderPassword;
-
-    @Value("${mail.transport.protocol}")
+    /*@Value("${mail.transport.protocol}")
     private String mailTransportProtocol;
 
     @Value("${mail.smtp.auth}")
@@ -36,27 +25,27 @@ public class MailConfiguration {
     private boolean mailSmtpStarttlsEnable;
 
     @Value("${mail.debug}")
-    private boolean mailDebug;
+    private boolean mailDebug;*/
 
     @Bean
     public JavaMailSender getJavaMailSender()
     {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(lbMailSenderHost);
+        /*mailSender.setHost(lbMailSenderHost);
         mailSender.setPort(lbMailSenderPort);
 
         if (mailSmtpAuth) {
             mailSender.setUsername(lbMailSenderUsername);
             mailSender.setPassword(lbMailSenderPassword);
-        }
+        }*/
 
 
 
-        Properties props = mailSender.getJavaMailProperties();
+        /*Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", mailTransportProtocol);
         props.put("mail.smtp.auth", mailSmtpAuth);
         props.put("mail.smtp.starttls.enable", mailSmtpStarttlsEnable);
-        props.put("mail.debug", mailDebug);
+        props.put("mail.debug", mailDebug);*/
 
         return mailSender;
     }
