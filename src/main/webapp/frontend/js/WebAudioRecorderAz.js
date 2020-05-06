e5119fd200fc9fb6d9b59e6fa07b5c0ad8807310
Igor Azarny <iazarny@yahoo.com>
2020-05-06 21:31:11 +0300

    Audio recording in production is fixed

--- src/main/webapp/frontend/js/WebAudioRecorderAz.js
+++ src/main/webapp/frontend/js/WebAudioRecorderAz.js
@@ -13,20 +13,21 @@ var audioContext; //new audio context to help us record
 
 
 function startRecording() {
-	console.log("startRecording() called");
+	__log("startRecording() called");
 
 	/*
 		Simple constraints object, for more advanced features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
     
-    var constraints = { audio: true, video:false }
+    var constraints = { audio: true, video:false };
 
     /*
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
 
+	__log("startRecording() getUserMedia with ", constraints);
 	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
 		__log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
 
@@ -44,7 +45,7 @@ function startRecording() {
 		//assign to gumStream for later use
 		gumStream = stream;
 
-		console.log(">>>>>>>>>> stream ", stream);
+		__log("stream ", stream);
 		
 		/* use the stream */
 		input = audioContext.createMediaStreamSource(stream);
@@ -76,7 +77,6 @@ function startRecording() {
 		recorder.onComplete = function(recorder, blob) { 
 			__log("Encoding complete");
 			createDownloadLink(blob,recorder.encoding);
-			//encodingTypeSelect.disabled = false;
 		}
 
 		recorder.setOptions({
@@ -87,16 +87,15 @@ function startRecording() {
 	    });
 
 		//start the recording process
+		__log("Recording starting");
+
 		recorder.startRecording();
 
 		 __log("Recording started");
 
 	}).catch(function(err) {
 
-		__log("Error");
-	  	//enable the record button if getUSerMedia() fails
-    	//recordButton.disabled = false;
-    	//stopButton.disabled = true;
+		__log("Error. Get user media fails", err);
 
 	});
 
@@ -105,16 +104,20 @@ function startRecording() {
 
 var paid;
 function stopRecording(pid) {
-	console.log("stopRecording() called " + pid);
+	__log("stopRecording() called " + pid);
 	
 	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
+	var  mst = gumStream.getAudioTracks(); //MediaStreamTrack[]
+
+	__log("mst ", mst);
+
+	mst[0].stop();
 
 	paid = pid;
 
 
 	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
-	console.log("finishRecording() called");
+	__log("finishRecording() called");
 	recorder.finishRecording();
 
 	__log('Recording stopped');
